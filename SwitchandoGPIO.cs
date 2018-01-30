576d6c31a8d2aefa3cb74ca938bcef2117ab8873
Marco Realacci <info@fantasy2001.com>
2018-01-30 16:34:13 +0100

    First commit

---
+++ SwitchandoGPIO.cs
@@ -0,0 +1,25 @@
+﻿using HomeAutomation.Objects.External.Plugins;
+using System;
+
+namespace Switchando.Plugins.GPIO
+{
+    public class SwitchandoGPIO : IPlugin
+    {
+        public string OnEnable()
+        {
+            return "";
+        }
+        public string GetName()
+        {
+            return "";
+        }
+        public string GetDeveloper()
+        {
+            return "";
+        }
+        public string GetDescription()
+        {
+            return "";
+        }
+    }
+}
